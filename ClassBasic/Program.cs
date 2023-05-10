40d832c6b1b8c02030ee2d5fa259fe6bc5c63070
Chaeyoung Moon <s2123@e-mirim.hs.kr>
2023-05-10 13:01:27 +0900

    math abs 추가

--- ClassBasic/Program.cs
+++ ClassBasic/Program.cs
@@ -4,8 +4,15 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+        class Math
+        {
+            public static int abs(int i) { return 0; }
+        }
+
         static void Main(string[] args)
         {
+            Math.abs(-3);
+
             OtherClass oc = new OtherClass();
 
             Console.WriteLine("Hello World!");
