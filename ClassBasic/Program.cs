8a05e6a2ac4a03b7d890d5e1dab22bd0d1aa98bb
Chaeyoung Moon <s2123@e-mirim.hs.kr>
2023-06-08 14:21:44 +0900

    리스트에서 인스턴스 생성 추가, 삭제

--- ClassBasic/Program.cs
+++ ClassBasic/Program.cs
@@ -15,7 +15,47 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
             OtherClass oc = new OtherClass();
 
+
             Console.WriteLine("Hello World!");
+
+            List<Student> list = new List<Student>()
+            {
+                new Student(){ name = "문채영", grade = 1 },
+                new Student(){ name = "김태연", grade = 2 },
+                new Student(){ name = "김하늘", grade = 3 },
+                new Student(){ name = "박나경", grade = 4 },
+                new Student(){ name = "장시우", grade = 1 },
+                new Student(){ name = "이연우", grade = 2 },
+            };
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("------------");
+            /*
+            foreach (var item in list)
+            {
+                if(item.grade > 1)
+                {
+                    list.Remove(item);
+                }
+            }*/
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                if (list[i].grade > 1)
+                {
+                    list.RemoveAt(i);
+                }
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+
         }
 
         class ThirdClass
---
+++ ClassBasic/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace MyApp
+{
+    public class Student
+    {
+        public string name;
+        public int grade;
+
+        public override string? ToString()
+        {
+            return this.grade + "학년 " + this.name;
+        }
+    }
+}
